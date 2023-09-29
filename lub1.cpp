c4cbef9273ecd6908c4072aca764998d3458c331
El1zavetaa <146248385+El1zavetaa@users.noreply.github.com>
2023-09-29 18:25:29 +0500

    Update lub1.cpp

--- lub1.cpp
+++ lub1.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
 #include <iomanip>
+#include <windows.h>
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	double a, b;
 	cout << "введите два числа: ";
 	cin >> a >> b;
